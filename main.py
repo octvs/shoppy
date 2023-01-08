a720e3dc4061045c52c41a196db9e552f3a1e288
octaskin <orhuncenktaskin@gmail.com>
2023-01-08 17:09:02 +0100

    filtering empty lines from user input

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     with open("categories.json", "r", encoding="utf-8") as f:
         item_map = reverse_dict(json.load(f))
 
+    # Read user input while filtering empty lines
     with open("input.txt", "r", encoding="utf-8") as f:
-        user_inp = f.read().splitlines()
+        user_inp = list(filter(None, f.read().splitlines()))
 
     print(f"User input read: {user_inp}")
     shopping_list = {}
