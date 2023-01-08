8e4571db66f8142efc04163bf6bf4bd7e4a246da
octaskin <orhuncenktaskin@gmail.com>
2023-01-08 17:28:31 +0100

    reading method to implement item details + readme

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 """Some helper funcs for shoppy"""
 
+import json
+
 
 def print_dict(dct):
     """
@@ -15,6 +17,28 @@ def print_dict(dct):
 def reverse_dict(dct):
     """Reverse a dict"""
     return {i: k for k in dct for i in dct[k]}
+
+
+def read_user_input():
+    """
+    Reads user input from a txt file returns a dictionary separated into categories ignoring blank lines
+    """
+    with open("categories.json", "r", encoding="utf-8") as file:
+        item_map = reverse_dict(json.load(file))
+
+    # Read user input while filtering empty lines
+    with open("input.txt", "r", encoding="utf-8") as file:
+        user_inp = list(filter(None, file.read().splitlines()))
+
+    print(f"User input read: {user_inp}")
+    shopping_list = {}
+    for itm in user_inp:
+        ctg = item_map.get(itm.split(",")[0], "Undefined")
+        if not shopping_list.get(ctg):
+            shopping_list[ctg] = []
+        shopping_list[ctg].append(itm)
+
+    return shopping_list
 
 
 def print_to_md(res):
@@ -41,5 +65,7 @@ def print_to_md(res):
         for itm in sorted(itm_list):
             md_string += f"- [ ] {itm}\n"
         md_string += "\n"
+
+    print("Shopping list updated.")
     with open("shopping_list.md", "w", encoding="utf-8") as file:
         file.write(md_string)
--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import json
-
-from helpers import print_to_md, reverse_dict
+"""shoppy, a simple script as a minimalist replacement for shopping list apps"""
+from helpers import print_to_md, read_user_input
 
 if __name__ == "__main__":
-    with open("categories.json", "r", encoding="utf-8") as f:
-        item_map = reverse_dict(json.load(f))
-
-    # Read user input while filtering empty lines
-    with open("input.txt", "r", encoding="utf-8") as f:
-        user_inp = list(filter(None, f.read().splitlines()))
-
-    print(f"User input read: {user_inp}")
-    shopping_list = {}
-    for itm in user_inp:
-        ctg = item_map.get(itm, "Undefined")
-        if not shopping_list.get(ctg):
-            shopping_list[ctg] = []
-        shopping_list[ctg].append(itm)
-
+    shopping_list = read_user_input()
     print_to_md(shopping_list)
