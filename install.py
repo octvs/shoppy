cb684ece29e3149abfa030529887ff58b219b251
octaskin <orhuncenktaskin@gmail.com>
2023-01-11 02:37:57 +0100

    debug statements

--- install.py
+++ install.py
@@ -3,7 +3,9 @@ import shutil
 from pathlib import Path
 
 shoppy_exec = Path.home().joinpath(".local/bin/shoppy")
+print(shoppy_exec)
 if not shoppy_exec.exists():
+    print(Path(__file__).parent.joinpath("shoppy"))
     print("Copying executable to .local/bin")
     shutil.copy(shoppy_exec, Path(__file__).parent.joinpath("shoppy"))
 else:
