a29e226e22f55c3291838a758d94dc19f393db6f
octvs <orhuncenktaskin@gmail.com>
2024-01-06 23:22:52 +0100

    feat: add flake and direnv support

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Python application flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      runtimeDeps = with pkgs.python3Packages; [pyfzf termcolor];
+      buildDeps = with pkgs.python3Packages; [setuptools];
+    in {
+      packages = rec {
+        shoppy = pkgs.python3Packages.buildPythonApplication {
+          pname = "shoppy";
+          version = "0.1";
+          pyproject = true;
+          propagatedBuildInputs = runtimeDeps ++ buildDeps;
+          src = ./.;
+        };
+        default = shoppy;
+      };
+
+      devShells.default = pkgs.mkShell {buildInputs = runtimeDeps;};
+    });
+}
