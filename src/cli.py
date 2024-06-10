03ffd04fd0d0c4b2c6edf20134729088e0b22ea6
octvs <orhuncenktaskin@gmail.com>
2024-06-10 22:37:32 +0200

    feat: migrate to argparse instead of raw parsing
    - Use argparse to parse cli arguments with flags, as an alternative to
      the previous hand-rolled argument parsing

--- src/cli.py
+++ src/cli.py
@@ -1,39 +1,40 @@
+import argparse
 import logging
 import os
-import sys
 
 from main import ShoppingList
 
-short_cmds = {"c": "check", "ls": "list", "e": "edit", "a": "add"}
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="verbose output"
+    )
 
-def parse_args(args: list) -> list:
-    if "--debug" in args:
-        logging.basicConfig(level=logging.DEBUG)
-        args.remove("--debug")
-    if len(args) == 1:
-        args.append("list")
-    if long_version := short_cmds.get(args[1]):
-        args[1] = long_version
-    return args
-
-
-def main(args=sys.argv):
-    args = parse_args(args)
-    logging.debug(f"Received args from shell {args}")
     shopping = ShoppingList()
 
-    match args[1]:
-        case "add":
-            shopping.add_item()
-        case "check":
-            shopping.check_item()
-        case "list":
-            print(shopping)
-        case "edit":
-            os.system(f"$EDITOR {shopping.fpath}")
-        case _:
-            exit("Unexpected argument!")
+    cmds = parser.add_subparsers(title="commands", dest="f")
+    cmds.add_parser(
+        "add", aliases=["a"], help="add new item to the list"
+    ).set_defaults(f=shopping.add_item)
+    cmds.add_parser(
+        "check", aliases=["c"], help="check an item from the list"
+    ).set_defaults(f=shopping.check_item)
+    cmds.add_parser(
+        "list", aliases=["l"], help="display the shopping list"
+    ).set_defaults(f=lambda: print(shopping))
+    cmds.add_parser(
+        "edit", aliases=["e"], help="open the list on $EDITOR"
+    ).set_defaults(f=lambda: os.system(f"$EDITOR {shopping.fpath}"))
+
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG, force=True)
+    logging.debug(f"Received args from shell {args}")
+    if args.f is None:
+        args.f = lambda: print(shopping)
+
+    args.f()
 
 
 if __name__ == "__main__":
