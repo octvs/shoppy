c2fb87ce849f1edcbe0641cdd66ba599eec868a5
octvs <octvs@posteo.de>
2025-12-05 13:47:24 +0100

    build: flake: migrate to flake-parts
    With this also retire `devShells` and `overlays`. The former is not
    needed since `direnv` already installs the package to PATH when needed.
    The latter is not used since it is consumed directly as flake.

--- flake.nix
+++ flake.nix
@@ -3,36 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
+    flake-parts,
+    systems,
     ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      py3 = pkgs.python3;
-      runtimeDeps = with py3.pkgs; [pyfzf termcolor] ++ [pkgs.jq];
-    in {
-      packages.default = pkgs.python3Packages.buildPythonApplication {
-        pname = "shoppy";
-        version = "0-unstable";
-        pyproject = true;
-        src = ./.;
-        build-system = with py3.pkgs; [setuptools];
-        dependencies = runtimeDeps;
-        nativeBuildInputs = [pkgs.installShellFiles];
-        postInstall = ''
-          installShellCompletion ${./src/_shoppy}
-        '';
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} ({...}: {
+      systems = import systems;
+      perSystem = {pkgs, ...}: let
+        dependencies = with pkgs.python3.pkgs; [pyfzf termcolor] ++ [pkgs.jq];
+      in {
+        packages.default = pkgs.python3Packages.buildPythonApplication {
+          pname = "shoppy";
+          version = "0-unstable";
+          pyproject = true;
+          src = ./.;
+          build-system = with pkgs.python3.pkgs; [setuptools];
+          inherit dependencies;
+          nativeBuildInputs = [pkgs.installShellFiles];
+          postInstall = ''
+            installShellCompletion ${./src/_shoppy}
+          '';
+        };
       };
-
-      devShells.default = pkgs.mkShell {buildInputs = runtimeDeps;};
-    })
-    // {
-      overlays.default = final: prev: {shoppy = self.packages.${prev.system}.default;};
-    };
+    });
 }
